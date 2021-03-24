98bddf5bb1842102ab42cb00c5438aabcf76977e
Tommas Meeussen <tommas.meeussen@hotmail.com>
2021-03-24 13:53:04 -0400

    Added Work Timer Scene

---
+++ Work Hard Play Hard/TimerScene.swift
@@ -0,0 +1,56 @@
+//
+//  TimerScene.swift
+//  Work Hard Play Hard
+//
+//  Created by Tommas Meeussen on 2021-03-24.
+//  Copyright © 2021 tm. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SpriteKit
+
+class TimerScene: SKScene, UITextFieldDelegate{
+    
+    var lblTimerTitle = SKLabelNode()
+    var lblTimer = SKLabelNode()
+    var lblStartTimer = SKLabelNode()
+    var lblPauseTimer = SKLabelNode()
+    var lblStopTimer = SKLabelNode()
+    var lblStartGame = SKLabelNode()
+    
+     override func didMove( to view: SKView){
+            self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+            
+            lblTimerTitle = self.childNode(withName: "lblTimerTitle") as! SKLabelNode
+
+            lblTimer = self.childNode(withName: "lblTimer") as! SKLabelNode
+            
+            lblStartTimer = self.childNode(withName: "lblStartTimer") as! SKLabelNode
+            
+            lblPauseTimer = self.childNode(withName: "lblPauseTimer") as! SKLabelNode
+            
+            lblStopTimer = self.childNode(withName: "lblStopTimer") as! SKLabelNode
+            
+            lblStartGame = self.childNode(withName: "lblStartGame") as! SKLabelNode
+
+            
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for t in touches{
+            let touchLocation = t.location(in: self)
+            
+            //Navigation to TimerScene when user clicks Login
+            if atPoint(touchLocation).name == "lblStartGame"{
+               
+
+                let gameScene = SKScene(fileNamed: "GameScene")
+                gameScene?.scaleMode = .aspectFit
+                view?.presentScene(gameScene!)
+            }
+        }
+    }
+    
+    
+}
